afe2edb98aba2db64bf6486ddd2f5ba9b65c11cf
Riza Ramadan <riza.ramadan@gmail.com>
2020-06-23 23:19:37 +0700

    skeleton board list

--- src/views/BoardList.vue
+++ src/views/BoardList.vue
@@ -1,15 +1,41 @@
 <template>
-  <div class="home">
-    <h1>This is board list page</h1>
+  <div id="board-list">
+    <a-row :gutter="16">
+      <a-col :span="6">
+        <a-card title="Card title" :bordered="false">
+          <p>card content</p>
+        </a-card>
+      </a-col>
+      <a-col :span="6">
+        <a-card title="Card title" :bordered="false">
+          <p>card content</p>
+        </a-card>
+      </a-col>
+      <a-col :span="6">
+        <a-card title="Card title" :bordered="false">
+          <p>card content</p>
+        </a-card>
+      </a-col>
+      <a-col :span="6">
+        <a-card title="Card title" :bordered="false">
+          <p>card content</p>
+        </a-card>
+      </a-col>
+    </a-row>
   </div>
 </template>
 
-<style>
-#components-layout-demo-top .logo {
-  width: 120px;
-  height: 31px;
-  background: rgba(255, 255, 255, 0.2);
-  margin: 16px 24px 16px 0;
-  float: left;
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator'
+
+@Component
+export default class BoardList extends Vue {
+  @Prop() private msg!: string;
+}
+</script>
+
+<style scoped>
+#board-list {
+  padding: 20px
 }
 </style>
